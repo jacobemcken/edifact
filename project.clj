b46484ff7f702c3ed153780693b081852ab06683
Jacob Emcken <je@casalogic.dk>
2016-11-19 20:47:28 +0100

    Set up project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject edifact "0.1.0-SNAPSHOT"
+  :description "A library to work with edifact files: https://en.wikipedia.org/wiki/EDIFACT"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
